b2b8d1f512688bfba180fd9d3cce45f5c4e0c90b
Igor <verycooleagle@gmail.com>
2018-05-08 22:24:31 +0300

    Exetends build front. Attach pm2.

--- server_employee/front/build/build.js
+++ server_employee/front/build/build.js
@@ -2,39 +2,57 @@
 require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
+const fs = require('fs');
+const ora = require('ora')
+const path = require('path')
+const chalk = require('chalk')
+const shell = require('shelljs')
+const webpack = require('webpack')
+const config = require('../config')
+const webpackConfig = require('./webpack.prod.conf')
 
-var ora = require('ora')
-var path = require('path')
-var chalk = require('chalk')
-var shell = require('shelljs')
-var webpack = require('webpack')
-var config = require('../config')
-var webpackConfig = require('./webpack.prod.conf')
-
-var spinner = ora('building for production...')
-spinner.start()
+const spinner = ora('building for production...')
+spinner.start();
 
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 shell.rm('-rf', assetsPath)
 shell.mkdir('-p', assetsPath)
 shell.config.silent = true
-shell.cp('-R', 'assets/*', assetsPath)
+shell.cp('-R', 'assets/*')
+
+const attachToProd = async () => {
+    shell.rm('-rf', `${__dirname}/../../static`)
+    shell.mkdir('-p', `${__dirname}/../../static`)
+    shell.cp('-R', `${__dirname}/../dist/static`, `${__dirname}/../../`);
+    shell.cp('-R', `${__dirname}/../dist/index.html`, `${__dirname}/../../static/index.html`);
+    shell.cp('-R', `${__dirname}/../static/img/favicon.png`, `${__dirname}/../../static/img/favicon.png`);
+    shell.cp('-R', `${__dirname}/../static/img/index.ring-loading-gif.svg`, `${__dirname}/../../static/img/index.ring-loading-gif.svg`);
+    shell.cp('-R', `${__dirname}/../static/img/sidebar-1.jpg`, `${__dirname}/../../static/img/sidebar-1.jpg`);
+}
+
 shell.config.silent = false
 
-webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
-  if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n\n')
+webpack(webpackConfig, async function (err, stats) {
 
-  console.log(chalk.cyan('  Build complete.\n'))
-  console.log(chalk.yellow(
-    '  Tip: built files are meant to be served over an HTTP server.\n' +
-    '  Opening index.html over file:// won\'t work.\n'
-  ))
+    if (err) {
+        spinner.stop()
+        throw err;
+    }
+
+    await attachToProd();
+
+    spinner.stop()
+    process.stdout.write(stats.toString({
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+    }) + '\n\n')
+
+    console.log(chalk.cyan('  Build complete.\n'))
+    console.log(chalk.yellow(
+        '  Tip: built files are meant to be served over an HTTP server.\n' +
+        '  Opening index.html over file:// won\'t work.\n'
+    ))
 })
